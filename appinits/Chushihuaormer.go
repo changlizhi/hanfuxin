86a07002cb276c97ae59c9894ad5206b30466ed6
changlz <changlizhi@aliyun.com>
2017-08-22 14:25:45 +0800

    去除Hanfuxinormer

--- appinits/Chushihuaormer.go
+++ appinits/Chushihuaormer.go
@@ -20,10 +20,6 @@ func ormerdebug() {
 	// 设置orm是否为debug模式
 	Setormerdebug(Chushihuas[zf.Zfs.Ormdebug(false)].Zhi)
 }
-func Setormerdebug(boolstr string) {
-	orm.Debug, _ = strconv.ParseBool(boolstr)
-}
-
 func ormermoxing() {
 	// 注册所有的实体，这些实体全部都是在baserun里生成的，请使用自动生成再在这里添加，
 	// 后期这个初始化将拆分并自动生成
@@ -54,6 +50,9 @@ func ormershujuku() {
 		zfzhi.Zhi.Xx() +
 		Shujukus[zf.Zfs.Mingcheng(false)].Zhi
 	orm.RegisterDataBase(zf.Zfs.Default(true), Shujukus[zf.Zfs.Qudong(false)].Zhi, url)
+}
+func Setormerdebug(boolstr string) {
+	orm.Debug, _ = strconv.ParseBool(boolstr)
 }
 func Defaultormer() orm.Ormer {
 	return Ormerbyname(zf.Zfs.Default(true))
